b7274ba6fc0e5a27fc57e5930b6cd86ae7ed01cd
Jetictors <zhengxcfutures@gmail.com>
2018-05-03 01:48:28 +0800

    Site updated: 2018-05-03 01:48:27

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Thu May 03 2018 01:41:01 GMT+0800 (中国标准时间)
+// build time:Thu May 03 2018 01:48:17 GMT+0800 (中国标准时间)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
